a5023cb4fc0477a3755f2bde6ac52d415bc38957
Pushkar Dhakad <pushkar.dhakad@nagarro.com>
2021-02-17 02:38:10 +0530

    modified Exception Handling and File Operation class

--- solutions/ExceptionHandling.cs
+++ solutions/ExceptionHandling.cs
@@ -4,11 +4,147 @@ using System.Text;
 
 namespace solutions
 {
+
+    class InvalidInputException:ApplicationException
+    {
+        public InvalidInputException(string message) : base(message)
+        {
+
+        }
+    }
+    class PlayedGameMultipleTimeException: ApplicationException
+    {
+        public PlayedGameMultipleTimeException(string message) : base(message)
+        {
+
+        }
+    }
     class ExceptionHandling
     {
         static void Main(string []args)
         {
-            Console.WriteLine("HII");
+            int count = 0;
+            while(true)
+            {
+                
+                Console.WriteLine("Enter any number from 1 - 5");
+                Console.WriteLine("1) Enter even number ");
+                Console.WriteLine("2) Enter odd number ");
+                Console.WriteLine("3) Enter a prime number");
+                Console.WriteLine("4) Enter a negative number ");
+                Console.WriteLine("5) Enter zero");
+                int inputVal = Int32.Parse(Console.ReadLine());
+                try
+                {
+                    count += 1;
+                    if(count>5)
+                    {
+                        throw new PlayedGameMultipleTimeException("You had played game more than 5 times");
+                    }
+                    AcceptChoice(inputVal);
+                }
+                catch(InvalidInputException iie)
+                {
+                    Console.WriteLine(iie);
+                }
+                catch (PlayedGameMultipleTimeException pgmte)
+                {
+                    Console.WriteLine(pgmte);
+                    break;
+                }
+                
+                static void AcceptChoice(int inputVal1)
+                {
+
+                
+                    int inputVal2;
+                    switch (inputVal1)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter even number ");
+                            inputVal2 = Int32.Parse(Console.ReadLine());
+                            if (inputVal2 % 2 == 0)
+                            {
+                                Console.WriteLine("Success");
+                            }
+                            else
+                            {
+                                throw new InvalidInputException("Enter even num");
+                            }
+
+                            break;
+                        case 2:
+
+                            Console.WriteLine(" Enter odd number ");
+                            inputVal2 = Int32.Parse(Console.ReadLine());
+                            if (inputVal2 % 2 != 0)
+                            {
+                                Console.WriteLine("Success");
+                            }
+                            else
+                            {
+                                throw new InvalidInputException("Enter odd num");
+                            }
+
+                            break;
+
+
+                        case 3:
+                            Console.WriteLine(" Enter a prime number");
+                            inputVal2 = Int32.Parse(Console.ReadLine());
+                            int  i, m = 0, flag = 0;
+                            m = inputVal2 / 2;
+                            for (i = 2; i <= m; i++)
+                            {
+                                if (inputVal2 % i == 0)
+                                {
+                                    Console.Write("Number is not Prime.");
+                                    flag = 1;
+                                    throw new InvalidInputException("Enter even num");
+                                    break;
+                                }
+                            }
+                            if (flag == 0)
+                                Console.WriteLine("success.");
+                            
+                            break;
+                        case 4:
+
+                            Console.WriteLine(" Enter a negative number ");
+                            inputVal2 = Int32.Parse(Console.ReadLine());
+                            if (inputVal2 < 0)
+                            {
+                                Console.WriteLine("Success");
+                            }
+                            else
+                            {
+                                throw new InvalidInputException("Enter negative num");
+                            }
+
+                            break;
+                        case 5:
+
+                            Console.WriteLine(" Enter zero");
+                            inputVal2 = Int32.Parse(Console.ReadLine());
+                            if (inputVal2 == 0)
+                            {
+                                Console.WriteLine("Success");
+                            }
+                            else
+                            {
+                                throw new InvalidInputException("Enter zero");
+                            }
+                            break;
+                        default:
+
+                            throw new ApplicationException("Enter valid choice");
+                            break;
+
+                    }
+
+                }
+
+            }
 
         }
     }
--- solutions/FileOperations.cs
+++ solutions/FileOperations.cs
@@ -6,48 +6,57 @@ namespace solutions
 {
     class FileOperations
     {
-        public static void Main(string[] args)
+        static int count = 0;
+        public static void Main(string[] arg)
         {
-            foreach (string path in args)
-            {
-                if (File.Exists(path))
+                String[] paths = { "C:\\Users\\HP\\Newrepository\\solutions", "C:\\Users\\HP\\Desktop\\nagarro\\learning materials\\C# Assignments 2019 (1)\\C# Material\\Day 4", "C:\\Users\\HP\\Desktop\\nagarro\\learning materials\\C# Assignments 2019 (1)" };
+
+             
+
+            foreach (string path in paths)
                 {
+                    if (File.Exists(path))
+                    {
                     // This path is a file
-                    ProcessFile(path);
-                }
-                else if (Directory.Exists(path))
-                {
+                    searchFile(path);
+                    }
+                    else if (Directory.Exists(path))
+                    {
                     // This path is a directory
-                    ProcessDirectory(path);
+                    searchDirect(path);
+                    }
+                    else
+                    {
+                        Console.WriteLine(path + " is not a valid path please provide correct path");
+                    }
+
                 }
-                else
-                {
-                    Console.WriteLine("{0} is not a valid file or directory.", path);
-                }
-            }
+            Console.WriteLine("total count of txt file is" + count);
+
+
         }
 
-        // Process all files in the directory passed in, recurse on any directories
-        // that are found, and process the files they contain.
-        public static void ProcessDirectory(string targetDirectory)
+        public static void searchDirect(string targetDirectory)
         {
             // Process the list of files found in the directory.
-            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            string[] fileEntries = Directory.GetFiles(targetDirectory, "*.txt");
             foreach (string fileName in fileEntries)
-                ProcessFile(fileName);
+                searchFile(fileName);
 
             // Recurse into subdirectories of this directory.
             string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
             foreach (string subdirectory in subdirectoryEntries)
-                ProcessDirectory(subdirectory);
+                searchDirect(subdirectory);
         }
 
         // Insert logic for processing found files here.
-        public static void ProcessFile(string path)
+        public static void searchFile(string path)
         {
-            Console.WriteLine("Processed file '{0}'.", path);
+            count += 1;
+            
+            Console.WriteLine("path of files are "+ path);
         }
-
+        
 
     }
 }
