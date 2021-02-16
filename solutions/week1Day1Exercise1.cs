a1e4e48f340e36a3d5014525bb9fc2abcbc44e7a
Pushkar Dhakad <pushkar.dhakad@nagarro.com>
2021-02-16 16:54:50 +0530

    added new class

---
+++ solutions/week1Day1Exercise1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace solutions
+{
+    class week1Day1Exercise1
+    {
+        class A
+        {
+            public int num;
+        }
+        static void Main()
+        {
+            Console.WriteLine("Pushkar Dhakad");
+         
+
+        
+
+            A objA = new A();
+            A objB = new A();
+            objA.num = 10;
+            objB.num = 20;
+            if (objA == objB)
+            {
+                Console.WriteLine("Reference are same");
+            }
+            else
+            {
+                Console.WriteLine("reference are not same");
+            }
+
+            if (objA.Equals(objB))
+            {
+                Console.WriteLine("content is same");
+            }
+            else
+            {
+                Console.WriteLine("content is not same");
+            }
+
+            objA = objB;
+
+            if (objA == objB)
+            {
+                Console.WriteLine("Reference are same");
+            }
+            else
+            {
+                Console.WriteLine("reference are not same");
+            }
+
+            if (objA.Equals(objB))
+            {
+                Console.WriteLine("content is same");
+            }
+            else
+            {
+                Console.WriteLine("content is not same");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+            String myName = "Pushkar";
+
+            String hisName = "Pushkar";
+
+            if (myName == hisName)
+            {
+                Console.WriteLine("Reference are same");
+            }
+            else
+            {
+                Console.WriteLine("reference are not same");
+            }
+            if (myName.Equals(hisName))
+            {
+                Console.WriteLine("content is same");
+            }
+            else
+            {
+                Console.WriteLine("content is not same");
+            }
+
+
+
+        }
+
+
+    }
+}
+
